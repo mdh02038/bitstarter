977dd8191117f010a34e7f886cec1ce1c4de9eba
Ubuntu <ubuntu@ip-172-31-32-143.us-west-2.compute.internal>
2013-07-05 19:52:27 +0000

    added missing req

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs      = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync( "index.html" );
+var text = buffer.toString();
+console.log("got " + text );
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
